7fc50bf56971acbc895cb48bb195b0e195cba20d
regilanmeira <regilan@hotmail.com>
2024-09-18 09:33:18 -0300

    Adiconei um H3

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <h1>Versinamento com GIT/GITHUB</h1>
+    <h3>Regilan atualizou o código</h3>
     <h2>Aula 11/09</h2>
     <p>Executando git commit</p>
     <a href="pagina1.php">Ir para página 1</a> 
